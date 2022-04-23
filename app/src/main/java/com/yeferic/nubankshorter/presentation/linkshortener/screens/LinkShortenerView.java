ffa72f9b8eebacae28b9eeeca9243601ab7bc0ed
yeferic <yeferic@gmail.com>
2022-04-23 10:28:53 -0500

    Define architecture

---
+++ app/src/main/java/com/yeferic/nubankshorter/presentation/linkshortener/screens/LinkShortenerView.java
@@ -0,0 +1,16 @@
+package com.yeferic.nubankshorter.presentation.linkshortener.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yeferic.nubankshorter.R;
+
+public class LinkShortenerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link_shortener);
+    }
+}
